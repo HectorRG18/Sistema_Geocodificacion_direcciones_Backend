76db4f208a2bfe2ed7396b59f8f8b2d073b25f26
Hector <ramirezgamonalhector@gmail.com>
2025-06-25 11:24:46 -0500

    Actualiza configuración en config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ DB_CONFIG = {
     'dbname': 'postgres',  # o 'bertDirection' si la creas
     'user': 'adminAddress',
     'password': 'Familiacruz01.',
-    'host': 'postgres-tesis.cbi4kw4gkfmm.us-east-2.rds.amazonaws.com',
+    'host': 'postgres-tesis3.cbi4kw4gkfmm.us-east-2.rds.amazonaws.com',
     'port': '5432'
 }
 
